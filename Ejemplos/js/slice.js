869d336e0bf364f9534cd768f31aa7accf91385d
= <=>
2023-06-07 10:45:34 -0500

    nuevas cosas

--- Ejemplos/js/slice.js
+++ Ejemplos/js/slice.js
@@ -1,5 +1,4 @@
+// Colocamos los parametros desde donde y hasta donde el texto
 const yo = "Pablooooo";
 const posicion = yo.slice(3, -2);
 console.log(posicion);
-
-// Colocamos los parametros desde donde y hasta donde el texto
---
+++ assets/js/arreglos.js
@@ -0,0 +1,104 @@
+let arreglo = new Array();
+let arr = [];
+
+let fruits = ["Papaya", "Piña", "Mango", "Fresa", "Mora", "Lulo"];
+console.log(fruits[0]);
+console.log(fruits[5]);
+console.log(fruits[3]);
+
+console.log(fruits);
+fruits[0] = "Coco";
+console.log(fruits);
+console.log(fruits[0])
+
+console.log(fruits.length)
+
+console.log(fruits[fruits.length - 1])
+console.log(fruits.at(-1)); // igual que la linea 16
+
+fruits.push("Papa"); // Inserta elemento al final del arreglo
+console.log(fruits)
+console.log(fruits.pop()); // Gets the first element and remove it
+console.log(fruits);
+
+
+console.log(fruits.shift()); //Extrae el primer elemento de un arreglo
+console.log(fruits)
+
+fruits.unshift("Mora", "Fresa"); // Agrega un elemento al comienzo del arreglo
+console.log(fruits);
+
+// Crear una funcion que agrege los nombre ingresado por parametro al arreglo names
+// Utilizar un promp segun use la funcion para agregar nombres
+// Escribir el codigo para insertar la cantidad que indique el usuario 
+
+
+// Mi manera
+
+// let people = [];
+// const num = parseInt(prompt("Numero de personas a agregar"))
+
+// function addPerson( name ){
+//     people.push(name);
+//     people.forEach( names => {
+//         console.log(names);
+//     });
+// }
+// let i = 0;
+// while( i < num ){
+//     let person = prompt("Nombre de la persona");
+//     addPerson(person);
+//     i ++;
+// }
+
+// Manera del instructor
+
+// let people = [];
+// function addName( name ){
+//     people.push(name);
+// }
+
+// let numNames = parseInt(prompt("¿Cuantos nombres quiere ingresar?"));
+
+// for ( let i = 0 ; i < numNames ; i ++){
+//     let nombre = prompt("Ingrese el nombre");
+//     addName(nombre);
+// }
+
+// for ( let i = 0 ; i < numNames ; i ++ ){
+//     console.log(people[i]);
+// }
+
+// // For .. of
+// for ( let nombre of people ){
+//     console.log(nombre);
+// }
+
+
+
+
+//Truncando el arreglo
+console.log(fruits.length = 3);
+console.log(fruits);
+
+
+delete fruits[1];
+console.log(fruits.length);
+console.log(fruits);
+
+// Slice
+fruits.splice(1, 1); // desde el indice 1, elimine 1 elemento
+console.log(fruits.length);
+console.log(fruits);
+
+
+arr = ["Yo", "estudio", "JS", "en", "el", "Sena"]
+console.log(arr);
+arr.splice(1, 2, "voy", "a", "bailar");
+console.log(arr);
+let datos = arr.splice(1, 3);
+console.log(datos);
+
+
+arr.splice(1, 0, "estudio", "JavaScript"); // Desde el indice 1, elimina 0 elementos y añado 2 desde la posicion 1;
+console.log(arr);
